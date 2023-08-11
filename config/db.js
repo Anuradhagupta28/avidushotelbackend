e54d7ae3d5b97cea004f8cec88357ba8a83344bc
Anuradhagupta28 <annugupta8512@gmail.com>
2023-08-11 13:28:23 +0530

    backend

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDatabase() {
     try{
-        await mongoose.connect('mongodb+srv://yashkumarpal987:Yash%4015182204@cluster0.ss532hp.mongodb.net/HotelPoint?appName=mongosh+1.8.0')
+        await mongoose.connect('mongodb+srv://annugupta8512:annu%408512@clusteranu.2ad3iyv.mongodb.net/avidushotel?appName=mongosh+1.8.0')
 
         console.log('Connected to DB')
 
